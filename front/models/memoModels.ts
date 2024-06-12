4d166b2f503eb8c7c171beeba595929739461d87
multios12 <5012052+multios12@users.noreply.github.com>
2024-06-12 20:11:16 +0900

    add: pwa manifest

--- front/models/memoModels.ts
+++ front/models/memoModels.ts
@@ -1,19 +1,2 @@
 export type memoType = { Id: string | undefined; Name: string; Date: string; Value: string; };
-export type settingType = {
-    Diary: { Name: string }
-    Categories: CategoryType[]
-}
-export type CategoryType = {
-    Key: string,
-    // 種類名
-    Name: string,
-    // 日付
-    Date: Date,
-    // 日付の使用・表示
-    UseDate: boolean,
-    // タグ表示 
-    UseTag: boolean,
-    // テンプレート
-    Template: string,
-    Fields: {}
-}
+

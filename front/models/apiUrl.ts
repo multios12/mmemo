b9851e1d78ff2fe1ca73df92b0cf69f71676c516
multios12 <5012052+multios12@users.noreply.github.com>
2025-08-31 11:17:47 +0900

    #14 ルーティングをsvelte5-routerに変更

--- front/models/apiUrl.ts
+++ front/models/apiUrl.ts
@@ -1,50 +1,50 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const getMonthApi = (month: string) => {
-  let url = `${apiUrl}/api/diary/${month}`;
+  let url = `${apiUrl}diary/${month}`;
   return fetch(url)
 }
 export const getDetailApi = (year: string, month: string, day: string) => {
-  let url = `${apiUrl}/api/diary/${year}/${month}/${day}`;
+  let url = `${apiUrl}diary/${year}/${month}/${day}`;
   return fetch(url)
 }
 export const postDetailApi = (year: string, month: string, day: string, body: string) => {
-  let url = `${apiUrl}/api/diary/${year}/${month}/${day}`;
+  let url = `${apiUrl}diary/${year}/${month}/${day}`;
   let options = { method: "post", body };
   return fetch(url, options)
 }
 export const deleteDetailApi = (year: string, month: string, day: string) => {
-  let url = `${apiUrl}/api/diary/${year}/${month}/${day}`;
+  let url = `${apiUrl}diary/${year}/${month}/${day}`;
   let options = { method: "delete" };
   return fetch(url, options)
 }
 
 export const getMemosSettingApi = () => {
-  let url = `${apiUrl}/api/memos`;
+  let url = `${apiUrl}memos`;
   return fetch(url);
 }
 export const getMemosApi = (category: string) => {
   category = category.replace("/", "");
-  let url = `${apiUrl}/api/memos/${category}`;
+  let url = `${apiUrl}memos/${category}`;
   return fetch(url);
 }
 export const getMemosIdApi = (category: string, id: string) => {
   category = category.replace("/", "");
-  let url = `${apiUrl}/api/memos/${category}/${id}`;
+  let url = `${apiUrl}memos/${category}/${id}`;
   return fetch(url);
 }
 export const putMemosApi = (category: string, memo: string) => {
-  let url = `${apiUrl}/api/memos/${category}`;
+  let url = `${apiUrl}memos/${category}`;
   let options = { method: "put", body: memo };
   return fetch(url, options);
 }
 export const postMemosApi = (category: string, id: string, memo: string) => {
-  let url = `${apiUrl}/api/memos/${category}/${id}`;
+  let url = `${apiUrl}memos/${category}/${id}`;
   let options = { method: "post", body: memo };
   return fetch(url, options);
 }
 export const deleteMemosIdApi = (category: string, id: string) => {
-  let url = `${apiUrl}/api/memos/${category}/${id}`;
+  let url = `${apiUrl}memos/${category}/${id}`;
   let options = { method: "delete" };
   return fetch(url, options);
 }
